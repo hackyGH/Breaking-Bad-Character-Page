90efdb36606e49dce20054a83c4efbc4b6164135
Gaurav Habad <gauravhabad113@gmail.com>
2021-06-21 00:32:06 +0530

    Updates

---
+++ precache-manifest.a7075765497205597d092707f13d4460.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645659064d22beda67a39f2207e28c06",
+    "url": "/Breaking-Bad-Character-Page/index.html"
+  },
+  {
+    "revision": "2b594b8a0507e1615e69",
+    "url": "/Breaking-Bad-Character-Page/static/css/main.0473b188.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/Breaking-Bad-Character-Page/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Character-Page/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b594b8a0507e1615e69",
+    "url": "/Breaking-Bad-Character-Page/static/js/main.53782093.chunk.js"
+  },
+  {
+    "revision": "2b2cb3115f08428f4109",
+    "url": "/Breaking-Bad-Character-Page/static/js/runtime-main.8e4802af.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Character-Page/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Character-Page/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Character-Page/static/media/spinner.235b9b3e.gif"
+  }
+]);
